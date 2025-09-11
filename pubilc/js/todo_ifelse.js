2313b5d60dd9fd6acfa73b991f6e16f11dd056e8
scv2780 <tjdcksgur1@naver.com>
2025-09-11 21:53:07 +0900

    조건문 todo

---
+++ pubilc/js/todo_ifelse.js
@@ -0,0 +1,20 @@
+/*
+* todo_ifelse.js
+*/
+function calculate() {
+  let first = parseInt(document.querySelector("#user1").value);
+  let last = parseInt(document.querySelector("#user2").value);
+  let opr = document.querySelector("#oper").value;
+  let result;
+  console.log(first, last, opr);
+  if (opr == '+') {
+    result = first + last;
+  } else if (opr == '-') {
+    result = first - last;
+  } else if (opr == '*') {
+    result = first * last;
+  } else {
+    result = first / last;
+  }
+  document.querySelector("#result").value = result;
+}
---
+++ pubilc/js/todo_switch.js
@@ -0,0 +1,21 @@
+/*
+* todo_switch.js
+*/
+function calculate() {
+  let first = parseInt(document.querySelector("#user1").value);
+  let last = parseInt(document.querySelector("#user2").value);
+  let opr = document.querySelector("#oper").value;
+  let result;
+  console.log(first, last, opr);
+  switch (opr) {
+    case '+':
+      result = parseInt(first) + parseInt(last); break;
+    case '-':
+      result = first - last; break;
+    case '*':
+      result = first * last; break;
+    case '/':
+      result = first / last; break;
+  }
+  document.querySelector("#result").value = result;
+}
