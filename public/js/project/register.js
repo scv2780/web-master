c25e8d72a28cb00fedab638819b9982b45ee8752
scv2780 <tjdcksgur1@naver.com>
2025-09-30 17:52:56 +0900

    0930 시험끝

---
+++ public/js/project/register.js
@@ -0,0 +1,36 @@
+    const form = document.getElementById("signupForm");
+    const passwordInput = document.getElementById("password");
+    const pwError = document.getElementById("pwError");
+
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+      let mId = document.querySelector('#userid').value;
+      let mPw = document.querySelector('#password').value;
+      let mNick = document.querySelector('#nickname').value;
+      let mName = document.querySelector('#name').value;
+      let mTel = document.querySelector('#tel').value;
+      let mMail = document.querySelector('#email').value;
+
+      fetch('http://localhost:3000/register', {
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+          },
+          body: JSON.stringify({
+            mId,
+            mPw,
+            mNick,
+            mName,
+            mTel,
+            mMail
+          })
+        })
+        .then(response => response.json())
+        .then((result) => {
+          console.log(result);
+        })
+        .catch((err) => console.log(err))
+
+      alert("회원가입이 완료되었습니다! 🎶");
+      form.reset();
+    });
